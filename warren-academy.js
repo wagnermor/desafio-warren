e93bf4474b11b3e45543fad723044ab7f0f1a5dd
WagnerMor <wagnermor@outlook.com>
2022-05-08 14:54:49 -0300

    Fix: desafio 03

--- warren-academy.js
+++ warren-academy.js
@@ -12,6 +12,8 @@ function reverse(n) {
 	return reverses;
 };
 
+console.log(`Desafio 01:\n ${reverse(27)}`);
+
 /*Desafio 02
 Um professor de programação, frustrado com a falta de disciplina de seus alunos, decidi cancelar a aula se menos de x alunos estiverem presentes quando a aula for iniciada. O tempo de chegada varia entre:
 
@@ -30,11 +32,13 @@ Aula normal.
 
 Explicação:
 Os três primeiros alunos chegaram no horário. Os dois últimos estavam atrasados. O limite é de três alunos, portanto a aula não será cancelada.*/
-function programmingClass(xStudents, arrivals) {
+function programming_class(xStudents, arrivals) {
 	let count = 0;
 	for(let arrival of arrivals) if(arrival <= 0) count++;
 	return count >= xStudents ? 'Aula normal' : 'Aula cancelada';
 };
+
+console.log(`\nDesafio 02:\n ${programming_class(3, [-2, -1, 0, 1, 2])}\n`);
 
 /*Desafio 03
 Dado um vetor de números e um número n. Determine a soma com o menor número de elementos entre os números do vetor mais próxima de n e também mostre os elementos que compõem a soma. Para criar a soma, utilize qualquer elemento do vetor uma ou mais vezes.
@@ -55,46 +59,50 @@ Saída de dados:
 Explicação:
 
 Se N = 10 e V = [2, 3, 4] você pode utilizar as seguintes soma: [2, 2, 2, 2, 2], [2, 2, 3, 3], [2, 4, 4] ou [3, 3, 4]. Como a quantidade de elementos em [2, 4, 4] e [3, 3, 4] é igual, os dois conjuntos devem ser mostrados.*/
-function vectorSum(n, v) {
-	let start = v[0];//2
-	let stop = v[v.length - 1]//4
-	let control = start;
-	const target = n;//10
-	const arrays = [];
-	let result = [];
+
+function vector_sum(n, v) {
+
+	const print_all_sum_rec = function(target, v, current_sum, start, result, output) {
+		const stop = v[v.length - 1];
+		if (current_sum === target) {
+			output.push(result.slice());
+		}
 	
-	console.log(`Start: ${start}`)
-	console.log(`Stop: ${stop}`)
-	
-	let current_sum = 0;
-	let temp_sum;
-	for(let i = start; i <= stop; i++) {
-		temp_sum = 0;
-		result = []
-		let j = i
-		temp_sum = current_sum + i;
-		console.log(`temp_sum: ${temp_sum}`)
-		while(temp_sum <= target) {
-			console.log(`temp_sum: ${temp_sum}`)
-			result.push(j)
-			temp_sum += j;
-			current_sum += j
-			if(current_sum === target) {
-				arrays.push(result)
+		for (let i = start; i <= stop; i++) {
+			let temp_sum = current_sum + i;
+			if (temp_sum <= target) {
+					result.push(i);
+					print_all_sum_rec(target, v, temp_sum, i, result, output);
+					result.pop();
+			} else {
+					return;
 			}
 		}
-		console.log(`result: ${result}`);
+	};
+	
+	const print_all_sum = function(target, v) {
+		const output = [];
+		const result = [];
+		print_all_sum_rec(target, v, 0, v[0], result, output);
+		return output;
+	};
+
+	const print_min_len = function() {
+		print_all_sum(n, v);
+		const minLength = print_all_sum(n, v).reduce((prev, curr) => prev.length = curr.length);
+		const arrays = print_all_sum(n, v).filter(item => item.length === minLength);
+
+		console.log(n);
+		arrays.map(item => console.log(item));
+		return arrays
 	}
-	console.log(`arrays: ${arrays}`);
-
-
-	return arrays;
-};
-
-console.log(vectorSum(10, [2,3,4]));
+	return print_min_len();
+}
+console.log('Desafio 03:');
+vector_sum(10, [2,3,4]);
 
 module.exports = {
 	reverse,
-	programmingClass,
-	vectorSum
+	programming_class,
+	vector_sum
 }
